19148ad39c8b494162159e4857e893f253cabed0
Lammirea <ardalin.sun@mail.ru>
2025-04-29 10:22:02 +0300

    change credentials

--- DevOps/Linux/CI/main.groovy
+++ DevOps/Linux/CI/main.groovy
@@ -2,7 +2,7 @@ pipeline {
     agent any
 
     environment {
-        DOCKERHUB_CREDS = credentials('big_data_second')
+        DOCKER_CREDS = credentials('big_data_lab_second')
     }
 
     options {
